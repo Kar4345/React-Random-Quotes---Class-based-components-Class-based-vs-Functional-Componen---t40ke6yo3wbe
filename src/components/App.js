d66e078ac110eb3a2718eb1a7e80208b99c3aae3
Kar4345 <kartik21gr@gmail.com>
2022-10-04 17:13:59 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 
 var colors = [
@@ -17,11 +17,26 @@ var colors = [
 ];
 
 const App = () => {
+  const random = Math.floor((Math.random() * 100) % colors.length)
+  const [quote,setQuote] = useState("");
+  const [author,setAuthor] = useState("");
+  const [background,setBackground] = useState(colors[random]);
+  async function getAuthorQuote(){
+    const infoJson = await fetch("https://api.quotable.io/random");
+    const data = await infoJson.json();
+    setAuthor(data.author);
+    setQuote(data.content)
+    setBackground(colors[random])
+    console.log(data)
+    console.log(background)
+  }
 
     return (
-      <div id="main">
-        <div id="wrapper">
-          
+      <div id="main" style={{backgroundColor:`${background}`}}>
+        <div id="wrapper" >
+          <div className="quote-text">{quote}</div>
+          <div className="quote-author">{author}</div>
+          <button id="new-quote" onClick={getAuthorQuote}>Fetch</button>
         </div>
       </div>
     );
